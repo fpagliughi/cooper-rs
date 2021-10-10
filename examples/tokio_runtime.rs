4f65ed4eb0210f4b70a29c8bc4445686105b502d
Simon Walker <s.r.walker101@googlemail.com>
2021-10-10 02:16:46 +0100

    Adds tokio specific example
    This example _requires_ the use of the `tokio` runtime, as it uses
    `reqwest` internally. We print a helpful message when not using the
    `tokio-rt` feature, and otherwise fetches a JSON response from
    `httpbin.org` and prints it.

---
+++ examples/tokio_runtime.rs
@@ -0,0 +1,51 @@
+use serde_json::Value;
+
+#[derive(Clone)]
+struct State {
+    client: reqwest::Client,
+}
+
+#[derive(Clone)]
+struct SharedClient {
+    actor: cooper::Actor<State>,
+}
+
+impl SharedClient {
+    pub fn new() -> Self {
+        Self {
+            actor: cooper::Actor::new(State {
+                client: reqwest::Client::new(),
+            }),
+        }
+    }
+
+    pub async fn fetch(&self, url: impl Into<String>) -> Value {
+        let url = url.into();
+        self.actor
+            .call(|_, state| {
+                Box::pin(async move {
+                    let res = state.client.get(url).send().await.unwrap();
+                    Some(res.json().await.unwrap())
+                })
+            })
+            .await
+    }
+}
+
+#[cfg(not(feature = "tokio-rt"))]
+#[tokio::main]
+async fn main() {
+    println!(
+        "This example fails to run with default features, 
+as it uses `reqwest` internally which in turn
+only runs on a tokio runtime."
+    );
+}
+
+#[cfg(feature = "tokio-rt")]
+#[tokio::main]
+async fn main() {
+    let client = SharedClient::new();
+    let res = client.fetch("https://httpbin.org/json").await;
+    dbg!(res);
+}
